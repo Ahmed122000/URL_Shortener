c6c1f50c61260e33e69a897de033640902da92df
Ahmed Hesham <67082298+Ahmed122000@users.noreply.github.com>
2024-12-21 23:45:38 +0200

    Delete server/Server/src/main/java/com/example/Server directory
    redundant code

--- server/Server/src/main/java/com/example/Server/repository/UrlRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Server.dao;
-
-import com.example.Server.entity.Url;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrlRepository extends JpaRepository<Url, String> {
-
-}
--- server/Server/src/main/java/com/example/Server/utility/Encoder.java
+++
@@ -1,4 +0,0 @@
-package com.example.Server.utility;
-
-public class Encoder {
-}
