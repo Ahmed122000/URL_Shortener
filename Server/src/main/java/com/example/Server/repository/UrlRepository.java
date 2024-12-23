7e12cefbaf24e24df2ac04207f862c67be2bb298
Ahmed Hesham <ahmedhesham122000@gmail.com>
2024-12-23 22:59:21 +0200

    Update and reformat the repostiory of the system

--- Server/src/main/java/com/example/Server/repository/UrlRepository.java
+++ Server/src/main/java/com/example/Server/repository/UrlRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface UrlRepository extends JpaRepository<Url, String> {
-    @Query(value = "SELECT * FROM url WHERE short_url = :shortUrl", nativeQuery = true)
-    Url getOriginalUrl(@Param("shortUrl") String shortUrl);
 
-    @Query(value = "SELECT full_url FROM url WHERE full_url = :fullUrl", nativeQuery = true)
-    String findFullUrl(@Param("fullUrl") String fullUrl);
+    Url findByFullUrl(String fullUrl);
 
+    @Query(value = "SELECT url_key FROM url;", nativeQuery = true)
+    List<String> findAllUrlKeys();
 }
 
