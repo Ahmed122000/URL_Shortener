7989708fe319c31dd469e47c68725e32334b86cf
Ahmed Hesham <ahmedhesham122000@gmail.com>
2024-12-23 23:51:46 +0200

    add the delete api & reformat the code for more readability

--- Server/src/main/java/com/example/Server/controller/URLShortenerController.java
+++ Server/src/main/java/com/example/Server/controller/URLShortenerController.java
@@ -4,18 +4,28 @@ package com.example.Server.controller;
 import com.example.Server.entity.Url;
 import com.example.Server.service.UrlService;
 import com.example.Server.utility.Encoder;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.HashSet;
+
 @RestController
 @RequestMapping("api/")
+@CrossOrigin(origins = "*")
 public class URLShortenerController {
 
-    UrlService urlService;
-    Encoder encoder;
+    //fixed value for the domain
+    private final static String CURRENT_DOMAIN = "http://localhost:8080/api/";
+    private int keyLength = 8;
+
+    private final UrlService urlService;
+    private final Encoder encoder;
+
+    private HashSet<String> keys;
 
     @Autowired
     public URLShortenerController(UrlService urlService, Encoder encoder) {
@@ -24,23 +34,64 @@ public class URLShortenerController {
     }
 
 
+    @PostConstruct
+    private void getAllKeys(){
+        keys = new HashSet<>(urlService.getAllKeys());
+    }
+
+
+    /**
+     * Shorten the provided URL
+     * @param fullUrl the long url provided by the user
+     * @return Http Response containing new Json file holding the (full url, short url, and the key)
+     */
     @PostMapping("/url")
-    public ResponseEntity<String> shortenUrl(@RequestBody String fullUrl){
-        Url url = urlService.getUrl(fullUrl);
+    public ResponseEntity<Url> shortenUrl(@RequestBody String fullUrl){
+
+        //Check if the URL is already shortened
+        Url url = urlService.getUrlInfoWithFullUrl(fullUrl);
         if(url != null)
-            return new ResponseEntity<>("localhost:8080/api/"+url.getShortUrl(), HttpStatus.OK);
+            return new ResponseEntity<>(url, HttpStatus.OK);
 
-        System.out.println("starting from beginning");
-        String shortUrl = encoder.hashUrl(fullUrl, 8);
-        Url tempUrl = new Url(fullUrl, shortUrl);
+        //Encode the input URL
+        String key = encoder.hashUrl(fullUrl, keyLength);
+
+        //Avoid key collision by checking if the key already exist
+        while(keys.contains(key)){
+            keyLength+= 2;
+            key = encoder.hashUrl(fullUrl, keyLength);
+        }
+        keys.add(key);
+
+        String shortUrl = CURRENT_DOMAIN + key;
+        Url tempUrl = new Url(key, fullUrl, shortUrl);
         urlService.saveUrl(tempUrl);
-        return new ResponseEntity<>("localhost:8080/api/"+shortUrl, HttpStatus.OK);
+
+        //return OK response with the new created URL
+        return new ResponseEntity<>(tempUrl, HttpStatus.CREATED);
     }
 
-    @GetMapping("/{shortUrl}")
-    public RedirectView redirect(@PathVariable String shortUrl){
-        Url originalUrl = urlService.getOriginalUrl(shortUrl);
-        return new RedirectView(originalUrl.getRealUrl());
+    /**
+     * Redirects to the original URL using the short URL key.
+     * @param key the key represents the short url
+     * @return a RedirectionView to the original full URL
+     */
+    @GetMapping("/{key}")
+    public ResponseEntity<RedirectView> redirect(@PathVariable String key){
+        Url originalUrl = urlService.findById(key);
+
+        if(originalUrl != null)
+            return new ResponseEntity<>(new RedirectView(originalUrl.getFullUrl()), HttpStatus.FOUND);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @DeleteMapping("/{key}")
+    public ResponseEntity<String> deleteUrl(@PathVariable String key){
+        if(keys.contains(key) || urlService.findById(key) != null){
+            urlService.deleteUrl(key);
+            keys.remove(key);
+            return new ResponseEntity<>("Url is deleted successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("URL not found", HttpStatus.NOT_FOUND);
+    }
 }
